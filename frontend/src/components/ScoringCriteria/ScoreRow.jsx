bc248d8eda65574c78681a516930d76561f283dc
namnguyenn99 <namnd.vnist@gmail.com>
2021-05-07 22:16:56 +0700

    replace id = name

--- frontend/src/components/ScoringCriteria/ScoreRow.jsx
+++ frontend/src/components/ScoringCriteria/ScoreRow.jsx
@@ -4,7 +4,7 @@ const ScoreRow = ({ index, score, handleScore, criterion }) => {
 
     return (
         <tr>
-            <th scope="row">{criterion.id}</th>
+            <th scope="row">{criterion.name}</th>
             <th><input className="form-control" type="number" name={`score${index}`} id={`score${index}`} min="0" max="100" id="expertScore" placeholder="Enter score..." value={score[index]} required onChange={(event) => handleScore(event, index, event.target.value)} /></th>
         </tr>
     );
